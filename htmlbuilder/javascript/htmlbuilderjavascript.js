a8a47b01b60e953858684334df636fd9d18f32cd
Sharik Madhyapradeshi <sharik.madhyapradeshi@gmail.com>
2020-12-27 06:48:49 +0530

    update css in js

--- htmlbuilder/javascript/htmlbuilderjavascript.js
+++ htmlbuilder/javascript/htmlbuilderjavascript.js
@@ -31,7 +31,7 @@ function getHTML() {
 	var title = document.getElementById('posttitle').value;
 	var htmlsourcecode = document.getElementById('sampleeditor').innerHTML;
 	if (htmlsourcecode != '' && htmlsourcecode != null) {
-		htmlsourcecode = '<!DOCTYPE html><html><head><title>'+title+'</title><style>ul {list-style-image: url("https://sharikmp.github.io/htmlbuilder/images/listarrow.png");}#postcontenttitle{border: 1px solid #ddd; padding:5px 5px 5px 5px;background:#6792EF;background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(132, 189, 255, 0.804359243697479) 100%);}#postcontent{background:rgb(255, 255, 255);//background:linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(132, 189, 255, 0.804359243697479) 100%);border:1px solid #ddd;padding:20px;font-family:arial;font-size:15px;}#maincontainer{padding-right:20px;padding-bottom:50px;padding-left:20px}.footer{left:0;bottom:5px;width:100%!important;min-width:300px;background:#6792EF;background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(132, 189, 255, 0.804359243697479) 100%);border: 1px solid #ddd;color:black;text-align:center;padding-top:20px;padding-bottom:1px}.footer a{color:black;text-decoration:underline;font-weight:bold}</style></head><body><div id="postcontent"><br/><div id="postcontenttitle" style="text-align: center;"><h3>' + title + '</h3></div><br/><br/><br/>' + htmlsourcecode;
+		htmlsourcecode = '<!DOCTYPE html><html><head><title>'+title+'</title><style>' + cssstyle() + '</style></head><body><div id="postcontent"><br/><div id="postcontenttitle" style="text-align: center;"><h3>' + title + '</h3></div><br/><br/><br/>' + htmlsourcecode;
 		htmlsourcecode += '<br/><br/><br/><br/><div class="footer"> <address> Posted on:' + new Date().toLocaleString() + '<br/>By <a id="developer" href="mailto:sharik.madhyapradeshi@gmail.com">Sharik Madhyapradeshi</a>.<br> The City Of Joy, West Bengal,<br> With Love From India </address></div>';
 		htmlsourcecode += '</div><br/></body></html>';
 		document.getElementById('test').innerHTML = htmlsourcecode;
@@ -50,3 +50,59 @@ function copyToClipboard(id) {
   document.execCommand("copy");
   document.getElementById('copiedMessage').innerHTML = 'Copied!';
 }
+
+function cssstyle(){ return `a {
+        color: black;
+      }
+      a:hover {
+        color: #fff;
+      }
+      ul {
+        list-style-image: url("https://sharikmp.github.io/htmlbuilder/images/listarrow.png");
+      }
+      #postcontenttitle {
+        border: 1px solid #ddd; 
+        padding:5px 5px 5px 5px;
+        background:#6792EF;
+        background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(132, 189, 255, 0.804359243697479) 100%);
+		font-family: arial;
+      }
+      #postcontent {
+        background:rgb(255, 255, 255);
+        //background:linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(132, 189, 255, 0.804359243697479) 100%);
+        border:1px solid #ddd;
+        padding:20px;
+		font-family: arial;
+        font-size:12px;
+        color:black;
+		line-height: 25px;
+      }
+      #maincontainer {
+        padding-right:20px;
+        padding-bottom:50px;
+        padding-left:20px;
+      }
+      .footer {
+        left:0;
+        bottom:5px;
+        width:100%!important;
+        min-width:300px;
+        background:#6792EF;
+        background: linear-gradient(270deg, rgba(255, 255, 255, 1) 0%, rgba(132, 189, 255, 0.804359243697479) 100%);
+        border: 1px solid #ddd;
+        color:black;
+        text-align:center;
+        padding-top:10px;
+        padding-bottom:10px;
+		font-family: arial;
+      }
+      .footer a {
+        color:black;
+        text-decoration:underline;
+        font-weight:bold;
+      }`;
+
+}
+
+
+
