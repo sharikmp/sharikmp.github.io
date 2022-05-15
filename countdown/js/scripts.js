13fea35885077aa9c82f6103f636c677600d3860
sharikmp <72480460+sharikmp@users.noreply.github.com>
2022-05-15 18:15:56 +0530

    Update scripts.js

--- countdown/js/scripts.js
+++ countdown/js/scripts.js
@@ -8,8 +8,9 @@ var percent;
 var rem;
 var animationTime = 2000;
 
+play();
+
 $(document).ready( () => {
-    play();
     now = new Date().getTime();
     countDownStartDate = new Date(startDate).getTime();
     countDownEndDate = new Date(endDate).getTime();
